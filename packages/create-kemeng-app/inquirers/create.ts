9cb00a2ff157abde928f98b05636064d04697ea3
wangzhan <wangzhan@bilibili.com>
2024-03-28 16:56:31 +0800

    feat: validate

--- packages/create-kemeng-app/inquirers/create.ts
+++ packages/create-kemeng-app/inquirers/create.ts
@@ -1,11 +1,31 @@
 import { input } from '@inquirer/prompts'
 
 export const createApp = async () => {
-	const projectName = await input({ message: 'project name: ' })
+	const projectName = await input({
+		message: 'project name: ',
+		transformer(value) {
+			return value.trim()
+		},
+		validate(value) {
+			if (!value) {
+				return 'Please enter the content'
+			}
+
+			return true
+		}
+	})
 
 	console.log('projectName:', projectName)
 
-	const firstAppName = await input({ message: 'first app name: ' })
+	const firstAppName = await input({
+		message: 'first app name: ',
+		validate(value) {
+			if (!value) {
+				return 'Please enter the content'
+			}
+			return true
+		}
+	})
 
 	console.log('firstAppName:', firstAppName)
 }
